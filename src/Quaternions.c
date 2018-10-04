4c103241197112e3de5054088d8ef1f3de180568
Mateusz Przybyla <inz.mateusz.przybyla@gmail.com>
2018-10-04 22:58:32 +0200

    Switched to pointers. Tests are commented out

--- src/Quaternions.c
+++ src/Quaternions.c
@@ -1,107 +1,120 @@
 #include "Quaternions.h"
 #include "MathUtils.h"
 
+#include <assert.h>
 #include <math.h>
 #include <stdio.h>
 
-Quat qempty() {
-    Quat q = {.s = 0.0, .v = vempty()};
-    q.is_normalized = false;
-    return q;
+void qempty(Quat* q) {
+    q->s = 0.0;
+    vempty(&q->v);
 }
 
-Quat qconj(const Quat* q) {
-    Quat qx = {.s = q->s, .v = vneg(&q->v)};
-    return qx;
+void qpure(Quat* q, const Vect* v) {
+    q->s = 0.0;
+    q->v = *v;
 }
 
-Quat qunit(const Quat* q) {
+void qreal(Quat* q, double s) {
+    q->s = s;
+    vempty(&q->v);
+}
+
+void qrotor(Quat* q, double theta, const Vect* axis) {
+    assert(axis->is_normalized && "Using non-versor axis for rotor");
+    q->s = cos(theta / 2.0);
+    q->v = *axis;
+    vscale(&q->v, sin(theta / 2.0));
+    q->is_normalized = true;
+}
+
+void qconjugate(Quat* q) {
+    vnegate(&q->v);
+}
+
+void qscale(Quat* q, double k) {
+    q->s = k * q->s;
+    vscale(&q->v, k);
+}
+
+int qnormalize(Quat* q) {
     if (q->is_normalized) {
-        return *q;
+        return 1;
     } else {
         double norm = qnorm(q);
         if (norm > 0.0) {
-            Quat qx = qscale(1.0 / norm, q);
-            qx.is_normalized = true;
-            return qx;
+            qscale(q, 1.0 / norm);
+            q->is_normalized = true;
+            return 0;
         } else {
-            return qempty();
+            return -1;
         }
     }
 }
 
-Quat qinv(const Quat* q) {
-    Quat qx = qconj(q);
+int qinverse(Quat* q) {
+    qconjugate(q);
     if (q->is_normalized) {
-        return qx;
+        return 0;
     } else {
         double norm_squared = qnormSquared(q);
-        return (norm_squared > 0.0) ? qscale(1.0 / norm_squared, &qx) : qempty();
+        if (norm_squared > 0.0) {
+            qscale(q, 1.0 / norm_squared);
+            return 0;
+        } else {
+            return -1;
+        }
     }
 }
 
-Quat qscale(double k, const Quat* q) {
-    Quat qx = {.s = k * q->s, .v = vscale(k, &q->v)};
-    return qx;
+void qadd(Quat* q1, const Quat* q2) {
+    q1->s = q1->s + q2->s;
+    vadd(&q1->v, &q2->v);
 }
 
-Quat qrotor(double theta, const Vect* axis) {
-    Vect normalized_axis = vnormal(axis);
-    Quat q = {.s = cos(theta / 2.0), .v = vscale(sin(theta / 2.0), &normalized_axis)};
-    q.is_normalized = true;
-    return q;
+void qsub(Quat* q1, const Quat* q2) {
+    q1->s = q1->s - q2->s;
+    vsub(&q1->v, &q2->v);
 }
 
-Quat qadd(const Quat* q1, const Quat* q2) {
-    Quat q = {.s = q1->s + q2->s, .v = vadd(&q1->v, &q2->v)};
-    return q;
-}
-
-Quat qsub(const Quat* q1, const Quat* q2) {
-    Quat q = {.s = q1->s - q2->s, .v = vsub(&q1->v, &q2->v)};
-    return q;
-}
-
-Quat qmul(const Quat* q1, const Quat* q2) {
+void qmul(Quat* q, const Quat* q1, const Quat* q2) {
     // q1 * q2 = [s1, v1] * [s2, v2] =
     // [s1 * s2 - dot(v1,v2), s1 * v2 + s2 * v1 + cross(v1,v2)]
-    double s = q1->s * q2->s - vdot(&q1->v, &q2->v);
+    q->s = q1->s * q2->s - vdot(&q1->v, &q2->v);
+    vcross(&q->v, &q1->v, &q2->v);
 
-    Vect v_part1 = vscale(q1->s, &q2->v);
-    Vect v_part2 = vscale(q2->s, &q1->v);
-    Vect v_part3 = vcross(&q1->v, &q2->v);
-    Vect v_part4 = vadd(&v_part1, &v_part2);
-    Vect v = vadd(&v_part3, &v_part4);
+    Vect v_aux = q1->v;
+    vscale(&v_aux, q2->s);
+    vadd(&q->v, &v_aux);
 
-    Quat q = {.s = s, .v = v};
-    return q;
+    v_aux = q2->v;
+    vscale(&v_aux, q1->s);
+    vadd(&q->v, &v_aux);
 }
 
-Quat qdiv(const Quat* q1, const Quat* q2) {
-    Quat q2_inv = qinv(q2);
-    return qmul(q1, &q2_inv);
+int qdiv(Quat* q, const Quat* q1, const Quat* q2) {
+    Quat q2_inv = *q2;
+    if (qinverse(&q2_inv) < 0) {
+        return -1;
+    }
+    qmul(q, q1, &q2_inv);
+    return 0;
 }
 
-Vect qpure(const Quat* q) {
-    Vect v = q->v;
-    v.is_normalized = false;
-    return v;
-}
-
-Vect qrotate(const Vect* v, const Quat* q) {
+void qrotate(Vect* v, const Quat* q) {
+    assert(q->is_normalized && "Using non-unit quaternion for rotation");
     Quat q_v = {.s = 0.0, .v = *v};
-    // TODO Consider removing this check and force user to provide proper rotor
-    Quat q_unit = qunit(q);
-    Quat q_inv = qinv(&q_unit);
+    Quat q_inv = *q;
+    if (qinverse(&q_inv) < 0) {
+        return;
+    }
 
-    Quat q_result = qmul(&q_unit, &q_v);
-    q_result = qmul(&q_result, &q_inv);
+    Quat q_result;
+    qmul(&q_result, q, &q_v);
+    qmul(&q_result, &q_result, &q_inv);
+
     q_result.is_normalized = v->is_normalized; // The norm of a vector is preserved
-    return q_result.v;
-}
-
-double qreal(const Quat* q) {
-    return q->s;
+    *v = q_result.v;
 }
 
 double qnorm(const Quat* q) {
--- src/Vectors.h
+++ src/Vectors.h
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 
 #define PACKED __attribute__((packed, aligned(1)))
+#define MUST_USE __attribute__((warn_unused_result))
 
 // 3D cartesian vector
 typedef struct PACKED Vect {
@@ -11,42 +12,58 @@ typedef struct PACKED Vect {
     bool is_normalized;
 } Vect;
 
-#undef PACKED
+// TODO: Review tests!
 
 // Construct an empty vector (0, 0, 0)
-Vect vempty();
+void vempty(Vect* v);
 
-// Get a negated version of a vector
-Vect vneg(const Vect* v);
+// Construct an X axis versor (1, 0, 0)
+void vversorX(Vect* v);
 
-// Get a normalized version of a vector
-Vect vnormal(const Vect* v);
+// Construct a Y axis versor (0, 1, 0)
+void vversorY(Vect* v);
 
-// Get a scaled version of a vector
-Vect vscale(double k, const Vect* v);
+// Construct a Z axis versor (0, 0, 1)
+void vversorZ(Vect* v);
 
-// Add two vectors
-Vect vadd(const Vect* v1, const Vect* v2);
+// Negate a vector
+void vnegate(Vect* v);
 
-// Subtract two vectors
-Vect vsub(const Vect* v1, const Vect* v2);
+// Scale a vector by a scalar
+void vscale(Vect* v, double k);
 
-// Get a cross product of two vectors
-Vect vcross(const Vect* v1, const Vect* v2);
+// Normalize a vector
+// Returns:
+//  1 if the vector was already normalized
+//  0 if the normalization was OK
+// -1 in case of failure
+int MUST_USE vnormalize(Vect* v);
 
-// Get a dot product of two vectors
-double vdot(const Vect* v1, const Vect* v2);
+// Add vector v2 to vector v1
+void vadd(Vect* v1, const Vect* v2);
+
+// Subtract vector v2 from vector v1
+void vsub(Vect* v1, const Vect* v2);
+
+// Get a cross product of two vectors: v1 x v2
+void vcross(Vect* v, const Vect* v1, const Vect* v2);
+
+// Get a dot product of two vectors: v1 * v2
+double MUST_USE vdot(const Vect* v1, const Vect* v2);
 
 // Get a norm of a vector
-double vnorm(const Vect* v);
+double MUST_USE vnorm(const Vect* v);
 
 // Get a squared norm of a vector
-double vnormSquared(const Vect* v);
+double MUST_USE vnormSquared(const Vect* v);
 
 // Compare two vectors by coordinates
-bool vcmp(const Vect* v1, const Vect* v2);
+bool MUST_USE vcmp(const Vect* v1, const Vect* v2);
 
 // Print a vector to stdout
 void vprint(const Vect* v);
+
+#undef PACKED
+#undef MUST_USE
 
 #endif // CTF_VECTORS_H_
