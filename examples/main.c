0b9f9210b9f312790fa70d5891b744be0033957f
Mateusz Przybyla <m.przybyla@fibargroup.com>
2019-12-05 08:21:16 +0100

    Start working on examples.

--- examples/main.c
+++ examples/main.c
@@ -1,63 +1,50 @@
 #include <SDL2/SDL.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <unistd.h>
 
+Sint32 x = -1;
+Sint32 y = -1;
+
 int main() {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        printf("SDL_Init Error: %s", SDL_GetError());
-        goto exit;
-    }
-
-    SDL_Window* win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
-    if (win == NULL) {
-        printf("SDL_Init Error: %s", SDL_GetError());
-        goto exit;
-    }
-
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window* win = SDL_CreateWindow("WTF?", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640,
+                                       480, SDL_WINDOW_SHOWN);
     SDL_Renderer* ren =
         SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    if (ren == NULL) {
-        printf("SDL_Init Error: %s", SDL_GetError());
-        SDL_DestroyWindow(win);
-        goto exit;
-    }
 
-    SDL_Surface* bmp = SDL_LoadBMP("/home/tysik/test.bmp");
-    if (bmp == NULL) {
-        printf("SDL_Init Error: %s", SDL_GetError());
-        SDL_DestroyRenderer(ren);
-        SDL_DestroyWindow(win);
-        goto exit;
-    }
+    while (true) {
+        SDL_Event e;
+        if (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT) {
+                break;
+            }
+            if (e.type == SDL_KEYDOWN) {
+                if (e.key.keysym.sym == SDLK_ESCAPE) {
+                    break;
+                }
+            }
+            if (e.type == SDL_MOUSEBUTTONDOWN) {
+                x = e.button.x;
+                y = e.button.y;
+            }
+        }
 
-    SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, bmp);
-    SDL_FreeSurface(bmp);
-    if (tex == NULL) {
-        printf("SDL_Init Error: %s", SDL_GetError());
-        SDL_DestroyRenderer(ren);
-        SDL_DestroyWindow(win);
-        goto exit;
-    }
-
-    // A sleepy rendering loop, wait for 3 seconds and render and present the screen each time
-    for (int i = 0; i < 3; ++i) {
-        // First clear the renderer
+        SDL_SetRenderDrawColor(ren, 0, 0, 0, SDL_ALPHA_OPAQUE);
         SDL_RenderClear(ren);
-        // Draw the texture
-        SDL_RenderCopy(ren, tex, NULL, NULL);
-        // Update the screen
+
+        SDL_SetRenderDrawColor(ren, 255, 255, 255, SDL_ALPHA_OPAQUE);
+        SDL_RenderDrawLine(ren, 320, 200, 300, 240);
+        SDL_RenderDrawLine(ren, 300, 240, 340, 240);
+        SDL_RenderDrawLine(ren, 340, 240, 320, 200);
+
+        SDL_RenderDrawPoint(ren, x, y);
+
         SDL_RenderPresent(ren);
-        // Take a quick break after all that hard work
-        SDL_Delay(1000);
     }
 
-    SDL_DestroyTexture(tex);
     SDL_DestroyRenderer(ren);
     SDL_DestroyWindow(win);
-    SDL_Quit();
-    return 0;
-
-exit:
     SDL_Quit();
     return 0;
 }
