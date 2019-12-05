6dc9c9458012edd60de94426ad36856b6fec5a61
Mateusz Przybyla <m.przybyla@fibargroup.com>
2019-12-05 23:28:01 +0100

    Add initial TF struct.

---
+++ include/types/TransformationType.h
@@ -0,0 +1,24 @@
+#ifndef WTF_TF_TYPE_H
+#define WTF_TF_TYPE_H
+
+#include "ScalarType.h"
+#include "VectorType.h"
+#include "RotationType.h"
+
+#define PACKED __attribute__((packed, aligned(1)))
+
+typedef union {
+    struct PACKED {
+        wtf_rot_t rotation;
+        wtf_vec_t translation;
+    };
+    struct PACKED {
+        wtf_vec_t i, j, k, t;
+    };
+    wtf_vec_t vectors[4];
+    wtf_scalar_t data[3][4];
+} wtf_tf_t;
+
+#undef PACKED
+
+#endif // WTF_TF_TYPE_H
